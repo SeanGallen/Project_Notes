ccd546248bfb7ff00bbcdd674ca79b0d3c294dfc
Sean <sjgallen@gmail.com>
2016-02-24 14:01:14 -0600

    fixed the first iteration test once again

--- test/acceptance_notes_test.rb
+++ test/acceptance_notes_test.rb
@@ -5,7 +5,7 @@ class NotesTest < Minitest::Test
 
   def test_displays_all_notes_by_default
 
-expected = 'Add 1 to 2  1 + 2  # =>
+expected = 'Add 1 to 2    1 + 2  # => 3
 Subtract 5 from 2    2 - 5  # => -3
 Is 1 less than 2    1 < 2  # => true
 Is 1 equal to 2    1 == 2 # => 3
@@ -19,8 +19,8 @@ Access an element in an array by its index    ["a","b","c"][0] # => "a"
 Find out how big the array is    ["a","b"].length # => 2
 '
 
-File.expand_path('../../bin/notes', __FILE__)
-stdout, stderr, exitstatus = Open3.capture3 'ruby', 'bin/notes'
+notes_program = File.expand_path('../../bin/notes', __FILE__)
+stdout, stderr, exitstatus = Open3.capture3 'ruby', notes_program
 assert_equal '', stderr
 assert exitstatus.success?
 assert_equal expected, stdout
