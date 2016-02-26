353357c0bc2fb6a84cb3815a2acd234982cdaed3
Matthew Swan <swanm86@gmail.com>
2016-02-26 09:18:37 -0600

    Add regex to acceptance_test so expected output can vary

--- test/acceptance_notes_test.rb
+++ test/acceptance_notes_test.rb
@@ -1,11 +1,10 @@
-require 'Open3'
-
+require 'open3'
 
 class NotesTest < Minitest::Test
 
   def test_displays_all_notes_by_default
 
-expected = 'Add 1 to 2    1 + 2  # => 3
+    expected = 'Add 1 to 2    1 + 2  # => 3
 Subtract 5 from 2    2 - 5  # => -3
 Is 1 less than 2    1 < 2  # => true
 Is 1 equal to 2    1 == 2 # => 3
@@ -19,13 +18,14 @@ Access an element in an array by its index    ["a","b","c"][0] # => "a"
 Find out how big the array is    ["a","b"].length # => 2
 '
 
-notes_program = File.expand_path('../../bin/notes', __FILE__)
-stdout, stderr, exitstatus = Open3.capture3 'ruby', notes_program
-assert_equal '', stderr
-assert exitstatus.success?
-assert_equal expected, stdout
+    notes_program = File.expand_path('../../bin/notes', __FILE__)
+    stdout, stderr, exitstatus = Open3.capture3 'ruby', notes_program
+    assert_equal '', stderr
+    assert exitstatus.success?
+    expected = /Convert 1 to a float *1.to_f # => 3/
+
+    assert_match expected, stdout
   end
 end
 
-expected_parsed_by_line = expected.scan(/^.*.$/)
 
