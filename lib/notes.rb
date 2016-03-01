51234f7e6f3e06db5c744b6348d1d241b19c0f83
YOUR NAME (Greg Glover) <YOUR EMAIL (101glover@gmail.com)>
2016-03-01 05:52:53 -0600

    coverted notes array to array of hashes to fix allignment

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,10 +1,12 @@
 class Notes
   def select(notes, selector)
-    notes.select { |note| note =~ /#{selector}/i }
+    notes.select { |note| note["descrip"] =~ /#{selector}/i }
   end
 
   def select_all(notes, selectors)
     selectors.each { |selector| notes = select(notes, selector) }
-    notes
+    notes.each do |position|
+      puts position["descrip"].ljust(1) + position["example"].rjust(100 - position["descrip"].length)
+    end
   end
 end
